c3ae3becb6eb831901efd108852da4d2154d51a6
Pablo_2e <pabloestebaneg0201@outlook.es>
2023-04-18 21:37:11 -0500

    Ejemplos de ciclos

--- prue.py
+++ prue.py
@@ -1,3 +1,21 @@
 print('prueba')
 
-a = 5
+a = 0
+print('Incio del ciclo')
+
+print(10<=10)
+
+while (a<10):
+    print('A')
+    a=a+1
+    print(a)
+
+print('fin del ciclo')
+
+print('Instrucciones')
+
+lista=[1,2,3,4,5,6,7,8,9,10]
+
+for i in lista:
+    print('El contenido de la lista es: ')
+    print(i)
